f2d505e1920d45f0ab387f098e6156d4713f1686
Arthur POIRET <dropsnorz@gmail.com>
2017-05-13 13:34:22 +0000

    Added mif37-dex local repo

--- src/main/java/com/dropsnorz/datamink/App.java
+++ src/main/java/com/dropsnorz/datamink/App.java
@@ -1,13 +1,24 @@
 package com.dropsnorz.datamink;
 
+import fr.univlyon1.mif37.dex.mapping.Mapping;
+import fr.univlyon1.mif37.dex.parser.MappingParser;
+import fr.univlyon1.mif37.dex.parser.ParseException;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ParseException
     {
         System.out.println( "Hello World!" );
+        
+        
+        MappingParser mp = new MappingParser(System.in);
+        Mapping mapping = mp.mapping();
+        
+        System.out.println(mapping.getEDB().size());
+        
+
     }
 }
