60647326f688bb919748b03c92a41c9414eaa636
Arthur POIRET <dropsnorz@gmail.com>
2017-06-09 16:41:34 +0000

    Fix program stratification

--- src/main/java/com/dropsnorz/datamink/core/DatalogProgram.java
+++ src/main/java/com/dropsnorz/datamink/core/DatalogProgram.java
@@ -107,21 +107,30 @@ public class DatalogProgram {
 			stratum.put(p, 0);
 		}
 
+		boolean change = true;
 
-		for(Tgd t : mapping.getTgds()){
-			String p = t.getRight().getName();
+		while (change){
 
-			for(Literal l : t.getLeft()){
-				if(!l.getFlag()){
-					int strat = Math.max(stratum.get(p), stratum.get(l.getAtom().getName()) + 1);
-					stratum.put(p, strat);
-				}
-				else{
-					int strat = Math.max(stratum.get(p), stratum.get(l.getAtom().getName()));
-					stratum.put(p, strat);
+			change = false;
+			
+			for(Tgd t : mapping.getTgds()){
+				String p = t.getRight().getName();
 
+				for(Literal l : t.getLeft()){
+					if(!l.getFlag()){
+						int strat = Math.max(stratum.get(p), stratum.get(l.getAtom().getName()) + 1);
+						if(stratum.get(p) != strat)change = true;
+						stratum.put(p, strat);
+					}
+					else{
+						int strat = Math.max(stratum.get(p), stratum.get(l.getAtom().getName()));
+						if(stratum.get(p) != strat)change = true;
+						stratum.put(p, strat);
+
+					}
 				}
 			}
+
 		}
 
 
@@ -199,7 +208,7 @@ public class DatalogProgram {
 		return g;
 
 	}
-	
+
 	public Mapping getMapping(){
 		return mapping;
 	}
